7837fc53bfd359a5efaee3788f12ad181d4b14e5
estagiarioFOrge <169064248+Gesshokuuyu@users.noreply.github.com>
2025-05-08 14:24:15 -0300

    Update UserController.php

--- Aulas/app/Http/Controllers/UserController.php
+++ Aulas/app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,7 +14,19 @@ class UserController extends Controller
     }
 
     public function store(Request $request) {
-            dd($request);
+            // dd($request);
+        try{
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+
+            return redirect()->route('user.create')->with('success', 'Usuario cadastrado com sucesso!');
+
+        }catch( Exception $e){
+            return redirect()->route('user.create')->with('error', 'Erro ao cadastrar usuario! ' . $e->getMessage());
+        }
         
     }
 }
